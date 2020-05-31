8fafc54a4b030498b5b79fdb86746d3793991461
Bishop-dev <alex.bishop@yandex.ru>
2020-06-01 02:31:25 +0300

    5 - our first project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'udemy-angular-styling-and-animations';
+  courseGoals = [
+    {title: 'Master Angular Styling', isActive: true},
+    {title: 'Understand Angular Animations', isActive: false},
+    {title: 'Master Angular Animations', isActive: false}
+  ];
 }
